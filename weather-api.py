cfab20ef997c4e54db0075ac23c784be839eb71d
Brody <45079557+brodyu@users.noreply.github.com>
2021-02-19 08:36:03 -1000

    OOP form

--- weather-api.py
+++ weather-api.py
@@ -1,8 +1,28 @@
 from wwo_hist import retrieve_hist_data
 
-frequency = 24
-start_date = '11-JAN-2010'
-end_date = '11-JAN-2021'
-api_key = 'c1146f76cc0c48e090552459211902'
-location_list = ['96816','96822']
-hist_weather_data = retrieve_hist_data(api_key, location_list, start_date, end_date, frequency, location_label = False, export_csv = True, store_df = True)
+# Credit to Ekapope Viriyakovithya's Medium article for the wwo_hist library
+class WorldWeatherOnline:
+
+    def __init__(self, frequency, start_date, end_date, api_key, location_list):
+        self.frequency = frequency
+        self.start_date = start_date
+        self.end_date = end_date
+        self.api_key = api_key
+        self.location_list = location_list
+
+    def call_api (self):
+        hist_weather_data = retrieve_hist_data(api_key = self.api_key, location_list = self.location_list, start_date = self.start_date, end_date = self.end_date, frequency = self.frequency, location_label = False, export_csv = True, store_df = True)
+
+if __name__ == "__main__":
+    # Data pull frequency by hour
+    frequency = 24
+    # Time horizon for data pull, remember limited to 500 API calls a day
+    start_date = '11-JAN-2020'
+    end_date = '11-JAN-2021'
+    # Input API key here:
+    api_key = '3e46143dc0e74b9eb56183102211902'
+    # Location list: input can be city name, zip code, state, country, etc
+    location_list = ['96789','96822']
+
+    model = WorldWeatherOnline(frequency, start_date, end_date, api_key, location_list)
+    model.call_api()
